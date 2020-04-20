a63245f7977b014840bd5de9d54f99ac37823065
Matthew Carras <matthew.carras+github@gmail.com>
2020-04-20 16:49:13 -0400

    Add -hf and -im options
    Add --headerformat FIELDNAME=FIELDTYPE and --ignorementions options. Revamp code to allow user given CSV fieldnames.
    
    Also switch from using faker.name to faker.user_name for Author field and mentions.

--- README.md
+++ README.md
@@ -1,22 +1,39 @@
 # discord_anonymize_chat_data
  Python project to anonymize chat text data (CSV) from Discord using Faker and optionally discord.py to connect to a given channel to collect new data from the channel's history. Currently anomymizes AuthorID, Author, and mentions in Content for each row of messages. Exports to UTF-8 (unicode) encoded CSV (if importing into Excel, make sure you select UTF-8 encoding). **Note that only mentions in the name#discriminator format will be anonymized.**
 
+ **In the latest version, the script supports input CSV files with any type of header if given a format for which fields to anonymize. See the new `-hf FIELDNAME=FIELDTYPE` option.** This parameter can be used to anonymize sources other than exported Discord chat data.
+ 
 ```
 usage: discord_anonymize_chat_data.py [-h] [-i CSV] [-o CSV] [-c CHANNELID]
-                                      [-t TOKEN]
+                                      [-t TOKEN] [-im]
+                                      [-hf FIELDNAME=FIELDTYPE [FIELDNAME=FIELDTYPE ...]]
 
 optional arguments:
   -h, --help            show this help message and exit
   -i CSV, --input CSV   anonymize existing CSV file
   -o CSV, --output CSV  ouput anonymized data to CSV file
   -c CHANNELID, --channelid CHANNELID
-                        CHANNELID to export history from. Both CHANNELID and
+                        discord CHANNELID to export history from. Both CHANNELID and
                         TOKEN are required to export history from an online
                         channel
   -t TOKEN, --token TOKEN
                         TOKEN required for connecting to discord (bot token)
-                        to export channel history. Both CHANNELID and TOKEN
+                        and exporting channel history. Both CHANNELID and TOKEN
                         are required to export history from an online channel
+  -im, --ignorementions
+                        Do not attempt to parse and replace mentions in the
+                        Content field when given an input file
+  -hf FIELDNAME=FIELDTYPE [FIELDNAME=FIELDTYPE ...], --headerformat FIELDNAME=FIELDTYPE [FIELDNAME=FIELDTYPE ...]
+                        Anonymize given header field name and type (multiples
+                        seperated by a space) instead of the default of
+                        AuthorID and Author. Field names and types are case
+                        insentitive. This parameter can be used to anonymize
+                        sources other than exported Discord chat data, and
+                        only affects a given CSV input file, also preserving
+                        its original header in the resulting output. Currently
+                        available types: Name,Number,Username. Example:
+                        [UserID=Number DisplayName=Name] will anonymize the
+                        UserID and DisplayName fields.
 ```
 
 Requires the Faker and discord.py libraries: `pip install Faker discord.py`
--- discord_anonymize_chat_data.py
+++ discord_anonymize_chat_data.py
@@ -2,8 +2,7 @@
 WIP to get and then anonymize a given Discord channel's chat history OR to read from a file of exisitng chat data
 Makes sure all mentions are exported with discriminators for ease of parsing
 Writes to exported_chat_data.csv in UTF-8 (unicode) format
-NOTE: Mentions must be in the name#discriminator format to be anonymized
-Last Updated: 4-15-20 by Matt Carras
+Last Updated: 4-20-20 by Matt Carras
 """
 
 import re 
@@ -11,6 +10,7 @@ import io
 from argparse import ArgumentParser
 from backports import csv
 from faker import Faker
+from faker.providers import internet
 from collections import defaultdict
 import discord
 
@@ -24,34 +24,58 @@ class Constants:
     # Initialize Faker and create mappings
     def __init__(self):
         faker = Faker()
-        self.fakerNames = defaultdict(faker.name)
-        self.fakerIds   = defaultdict(faker.random_number)
+        faker.add_provider(internet)
+        self.fakerNames     = defaultdict(faker.name)
+        self.fakerNumbers   = defaultdict(faker.random_number)
+        self.fakerUsernames = defaultdict(faker.user_name)
+        self.anonymizationDicts = { 
+            'name': self.fakerNames, 
+            'number': self.fakerNumbers,
+            'username': self.fakerUsernames
+        }
 consts = Constants()
 
+# Abort script, displaying message and doing any required clean-up first.
+def abort_script(msg):
+    print('ERROR: {0}'.format(msg))
+    quit()
+    
 # Anonymize a row of data, search through content for mentions and anonymizing them as well. 
 # Mentions must be in name#discriminator format.
 # Currently anonymizes the Author and AuthorID field, as well as mentions in content.
-def anonymize_row(row):
-    row['AuthorID'] = consts.fakerIds[row['AuthorID']]
-    row['Author'] = consts.fakerNames[row['Author']]
-    
+def anonymize_row(row,fieldnames,csvheaderformatdict=None,ignorementions=False):
+    # See if we're given a custom CSV header format
+    if not csvheaderformatdict:
+        row['AuthorID'] = consts.fakerNumbers[row['AuthorID']]
+        row['Author'] = consts.fakerUsernames[row['Author']]
+    else:
+        # FieldName: FieldType
+        for fieldname in fieldnames:
+            anondictname = csvheaderformatdict.get(fieldname.lower())
+            if anondictname:
+                row[fieldname] = consts.anonymizationDicts[anondictname][row[fieldname]]
+                
     # Search for mentions in Content row in format of name#discriminator
-    mentions = re.findall('@([^#]+#[0-9]+)', row['Content'])
-    for mention in mentions:
-        row['Content'] = row['Content'].replace(mention, consts.fakerNames[mention])
+    if not ignorementions:
+        mentions = re.findall('@([^#]+#[0-9]+)', row['Content'])
+        for mention in mentions:
+            row['Content'] = row['Content'].replace(mention, consts.fakerUsernames[mention])
     
 # Anonymize all rows of a given CSV file to destination file
-def anonymize_file(source,dest):
+def anonymize_file(source,dest,csvheaderformatdict=None,ignorementions=False):
     print('Reading from [{0}] and writing anonymized data to [{1}]...'.format(source,dest))
     with io.open(source, 'r', encoding='utf8') as f:
     #with io.open(source, 'r') as f:
         with io.open(dest, 'w', encoding='utf8') as o:
-            fieldnames = consts.defaultHeader
             reader = csv.DictReader(f)
+            if not csvheaderformatdict:
+                fieldnames = consts.defaultHeader
+            else:
+                fieldnames = reader.fieldnames
             writer = csv.DictWriter(o,fieldnames)
             writer.writeheader()
             for row in reader:
-                anonymize_row(row)
+                anonymize_row(row,fieldnames,csvheaderformatdict,ignorementions)
                 writer.writerow(row)
 
 # Create class for interacting with discord.py and the export_anonymized_channel_history method             
@@ -83,7 +107,7 @@ class DiscordClient(discord.Client):
                     for user in message.mentions:
                         row['Content'] = row['Content'].replace('<@!{0}>'.format(user.id),'@{0}#{1}'.format(user.name,user.discriminator))
                     
-                    anonymize_row(row)
+                    anonymize_row(row,fieldnames)
                     
                     writer.writerow(row)
         # Using await should wait until any previous async finishes
@@ -99,17 +123,41 @@ parser.add_argument("-o", "--output", dest="csvoutput",
                     help="ouput anonymized data to CSV file", metavar="CSV",
                     default=consts.defaultOutputFile)
 parser.add_argument("-c", "--channelid", dest="channelid",
-                    help="CHANNELID to export history from. Both CHANNELID and TOKEN are required to export history from an online channel", metavar="CHANNELID")
+                    help="discord CHANNELID to export history from. Both CHANNELID and TOKEN are required to export history from an online channel", metavar="CHANNELID")
 parser.add_argument("-t", "--token", dest="bottoken",
-                    help="TOKEN required for connecting to discord (bot token) to export channel history. Both CHANNELID and TOKEN are required to export history from an online channel", metavar="TOKEN")                    
+                    help="TOKEN required for connecting to discord (bot token) and exporting channel history. Both CHANNELID and TOKEN are required to export history from an online channel", metavar="TOKEN")
+parser.add_argument("-im", "--ignorementions", dest="ignorementions",
+                    help="do not attempt to parse and replace mentions in the Content field when given an input file",
+                    action='store_true', default=False)
+# Fancy format command below takes the keys from consts.anonymizationDicts.keys, capitlizes the first letter of each key, sorts them, and joins them to a single string, separated by commas
+parser.add_argument("-hf", "--headerformat", dest="csvheaderformat",
+                    nargs='+',
+                    help='anonymize given header field name and type (multiples seperated by a space) instead of the default of AuthorID and Author. Field names and types are case insentitive. This parameter can be used to anonymize sources other than exported Discord chat data, and only affects a given CSV input file, also preserving its original header in the resulting output. Currently available types: {0}. Example: [UserID=Number DisplayName=Name] will anonymize the UserID and DisplayName fields.'.format(','.join(sorted([s.capitalize() for s in consts.anonymizationDicts.keys()]))), metavar='FIELDNAME=FIELDTYPE')
+                    
 # parser.add_argument("-q", "--quiet",
                     # action="store_false", dest="verbose", default=True,
                     # help="don't print status messages to stdout")
 
 args = parser.parse_args()
 
+# If --headerformat argument given, convert from list to hash table / dictionary
+csvheaderformatdict = None
+if args.csvheaderformat:
+    print('Parsing given custom header format...')
+    csvheaderformatdict = {}
+    # Look for arguments in the given list in the format of FieldName=Type
+    for hformatstr in args.csvheaderformat:
+        splitstr = hformatstr.split('=', 1)
+        if splitstr:
+            fieldnamestr = splitstr[0].lower()
+            typestr = splitstr[1].lower()
+            # Double-check we have a valid type
+            if not typestr in consts.anonymizationDicts.keys():
+                abort_script('Invalid or unknown field type in [{0}]. Check -h for more information on valid field types.'.format(hformatstr))
+            csvheaderformatdict[ fieldnamestr ] = typestr
+    
 if args.csvinput:
-    anonymize_file(args.csvinput,args.csvoutput)
+    anonymize_file(args.csvinput,args.csvoutput,csvheaderformatdict,args.ignorementions)
 elif args.channelid and args.bottoken:
     client = DiscordClient()
     client.prepare_get_channel_history(exportChannelID=args.channelid,exportCSV=args.csvoutput,exportChannelLimit=None,exportChannelAfter=None)
@@ -118,6 +166,8 @@ elif args.channelid or args.bottoken:
     print('ERROR: Both TOKEN and CHANNELID are required to connect to discord')
 else:
     # Empty / invalid arguments
+    print('ERROR: Input source required (CSV file or directly from Discord)')
     parser.print_help()
-           
+
+# DC.MC^2 Channel ID = 176329233319198720
 
